f9de3c4e79df16cd8f4b3c17f6e3441f2f50dd74
lorran.mendes <lorran.mendes@pitang.com>
2021-01-24 22:57:28 -0300

    comit heroku

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/Projeto'));
+app.use(express.static(__dirname +'/dist'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/Projeto/index.html');
+res.sendFile(__dirname + 'dist/index.html');
 });
 
 
-app.listen(PORT, () => {
-  console.log('servidor na porta ' + PORT);
+app.listen(PORT, () =>{
+    console.log('server iniciado ' + PORT)
 })
